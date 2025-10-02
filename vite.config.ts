777ff160dd0cd7e47c438b5a575b36a65d7970e2
tanvisharmaaa <61910708+tanvisharmaaa@users.noreply.github.com>
2025-10-01 22:45:54 -0400

    fix assets path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/neural-visualizer/' : '/',
+  base: '/neural-visualizer/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
